95502bf251fa8695c6b12305698f157da97cc8cd
fmokoena <70021735+fmokoena@users.noreply.github.com>
2020-11-09 15:31:12 +0200

    Update help_cc.py
    added dictionary of valid commands and function to print

--- main/help_cc.py
+++ main/help_cc.py
@@ -1,1 +1,24 @@
 # THIS MODULE IMPLEMENTS A HELP COMMAND WITH A LIST OF ALL POSSIBLE
+
+
+# Defining of Global variables is done here
+
+list_of_commands = {
+"mbook" : "view and book availble time slots",
+"vbook" : "view bookings",
+"cbook" : "cancel booking",
+"mtslot" : "create time slot",
+"dtslot" : "displays time slots",
+"ctslot" : "cancel time slot"
+}
+
+# End of global variables
+
+def print_help():
+    global list_of_commands
+    print("Valid Commands:")
+    for i in list_of_commands:
+        print(f"{i} : {list_of_commands[i]}")
+
+if __name__ == "__main__":
+    print_help()
