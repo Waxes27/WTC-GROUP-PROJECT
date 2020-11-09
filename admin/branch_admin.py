9263c5b0215e4edc3e6cd7db9b8c4adc36778570
Nceba Dumasi <ndumasi@c5r8s5.co.za>
2020-11-09 18:27:24 +0200

    FINISH

--- admin/branch_admin.py
+++ admin/branch_admin.py
@@ -2,8 +2,8 @@ import os
 import sys
 import subprocess
 
-list_of_usernames = ['ndumasi','bidaniel','vpekane','sbaloyi',]
-
+list_of_usernames = ['ndumasi','bidaniel','vpekane','sbaloyi','apillay','nwalter','cdu-pree','dchappie','fmokoena','sigamede','tmentor','tmoshole','vsithole']
+list_ = ['ndumasi', 'tmentor', 'dchappie']
 
 """
 git checkout "branch name"
@@ -12,3 +12,12 @@ git merge Playground
 
 def number_of_usernames():
     return len(list_of_usernames)
+
+
+def qwer():
+    for i in range(number_of_usernames()):
+        for i in list_:
+            os.system(f"git checkout {i}")
+            os.system(f"git merge Playground")
+
+qwer()
