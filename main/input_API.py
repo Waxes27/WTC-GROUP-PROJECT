d125c34cfa6c5a69629b3af89e0946c90c66b625
Sam Baloyi <sambaloyi2017@gmail.com>
2020-11-12 09:22:18 +0200

    Update input_API.py

--- main/input_API.py
+++ main/input_API.py
@@ -14,28 +14,26 @@ topic_list = ["Recursion", "Unit Testing", "List Comprehensions", "Lambdas", ""]
 
 
 def get_role():
-    role = input('What do you wish your role to be for the code clinic? ').lower()
+    role = input('What do you wish your role to be for the code clinic?: ').lower()
     roles = ['doctor', 'patient']
     while len(role) == 0 or role not in roles:
         print('\nYou can either choose to be a \n(1) Doctor or \n(2) Patient.')
-        role = input('What do you wish your role to be for the code clinic? ').lower()
+        role = input('What do you wish your role to be for the code clinic?: ').lower()
     print('Role stored successfully.\n')
     return role
 
 
 def doctor_username(doctor_list):
-    booking_doc = input("Please provide the name of the Coding Clinician. " +
-    "\nType HELP for assistance\n").lower()
+    booking_doc = input("Please provide the name of the Coding Clinician: ").lower()
     
     while len(booking_doc) <= 0 or booking_doc not in doctor_list:
         print("Please provide a valid username.\n")
-        booking_doc = input("Please provide the name of the Coding Clinician\n").lower()
+        booking_doc = input("Please provide the name of the Coding Clinician: ").lower()
     return booking_doc + '@student.wethinkcode.co.za'
 
 
 def patient_username(patient_list):
-    booking_pat = input("Please provide the name of the Coding Patient. " +
-    "\nType HELP for assistance\n").lower()
+    booking_pat = input("Please provide the name of the Coding Patient: ").lower()
     
     while len(booking_pat) <= 0 or booking_pat not in patient_list:
         print("\nPlease provide a valid username.")
@@ -53,12 +51,12 @@ def print_topic_booking(role_username, booking_topic):
     role_username_topic = {role_username: booking_topic}
     print('\nTopic booked!\n\nDetails: ')
     for user,topic in role_username_topic.items():
-        print(f'Username: {user[0]} \nRole: {user[1]} \nTopic: {topic}')
+        print(f'Username: {user[0]} \nRole: {user[1].capitalize()} \nTopic: {topic}')
 
 def book_topic(topic_list, username, role):
     print_topics(topic_list)
 
-    booking_topic = input("Please choose a topic you would like to clinic? Or leave blank to choose a 'General' topic\n").capitalize()
+    booking_topic = input("Please choose a topic you would like to clinic? Or leave blank to choose a 'General' topic: ").capitalize()
     
     while booking_topic not in topic_list and booking_topic != "General":
         booking_topic = input("Please choose a valid topic from the list above:\n")
