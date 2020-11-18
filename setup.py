5c88415c5b1a2275be31e088e8de0d31443ab96e
Nceba Dumasi <ndumasi@c5r8s5.co.za>
2020-11-18 15:18:12 +0200

    not done testing apparently

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ import os
 import subprocess
 import time
 
-bashrc = open(f"{os.environ['HOME']}/.bashrc", 'a')
+zsh = open(f"{os.environ['HOME']}/.zshrc", 'a+')
+bashrc = open(f"{os.environ['HOME']}/.bashrc", 'a+')
+bashrc1 = open(f"{os.environ['HOME']}/.bashrc", 'r')
 
 print('Starting... Do not interrupt process')
 value = subprocess.getoutput("""pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
@@ -17,7 +19,13 @@ pip3 install datefinder
 cp -r WTC-GROUP-PROJECT ~/Music
 rm -rf WTC-GROUP-PROJECT
 git clone https://github.com/Waxes27/WTC-GROUP-PROJECT.git WTC-GROUP-PROJECT""")
-bashrc.write("\nalias clinic='python3 ~/WTC-GROUP-PROJECT/main/main.py'\n")
+    
+if "alias clinic" not in bashrc1.read():
+    bashrc.write("\nalias clinic='python3 ~/WTC-GROUP-PROJECT/main/main.py'\n")
+    zsh.write("\nalias clinic='python3 ~/WTC-GROUP-PROJECT/main/main.py'\n")
+
+
+
 os.system("""mkdir ~/.config/.clinic
 
 cp ~/WTC-GROUP-PROJECT/main/code/codebase/credentials.json ~/.config/.clinic
