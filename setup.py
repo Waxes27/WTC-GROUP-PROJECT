1d5411af1afb792f19d3a77f04ccb32a2dcd00e1
Nceba Dumasi <ndumasi@c5r8s5.co.za>
2020-11-18 14:50:12 +0200

    final update to setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ git clone https://github.com/Waxes27/WTC-GROUP-PROJECT.git WTC-GROUP-PROJECT""")
 if 'not found' in value:
     os.system("""pip3 install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 pip3 install datefinder
+cp -r WTC-GROUP-PROJECT ~/Music
+rm -rf WTC-GROUP-PROJECT
 git clone https://github.com/Waxes27/WTC-GROUP-PROJECT.git WTC-GROUP-PROJECT""")
 bashrc.write("\nalias clinic='python3 ~/WTC-GROUP-PROJECT/main/main.py'\n")
 os.system("""mkdir ~/.config/.clinic
