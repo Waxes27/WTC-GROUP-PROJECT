019eb29e941beee7f2a4453bfa6ce9b322825678
Nceba Dumasi <ndumasi@c5r8s5.co.za>
2020-11-18 14:45:31 +0200

    trying to fix setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ bashrc = open(f"{os.environ['HOME']}/.bashrc", 'a')
 print('Starting... Do not interrupt process')
 value = subprocess.getoutput("""pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 pip install datefinder
-git clone https://github.com/Waxes27/WTC-GROUP-PROJECT.git""")
+mkdir ~/WTC-GROUP-PROJECT
+git clone https://github.com/Waxes27/WTC-GROUP-PROJECT.git WTC-GROUP-PROJECT""")
 
 
 if 'not found' in value:
     os.system("""pip3 install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 pip3 install datefinder
-git clone https://github.com/Waxes27/WTC-GROUP-PROJECT.git ~/""")
+git clone https://github.com/Waxes27/WTC-GROUP-PROJECT.git WTC-GROUP-PROJECT""")
 bashrc.write("\nalias clinic='python3 ~/WTC-GROUP-PROJECT/main/main.py'\n")
 os.system("""mkdir ~/.config/.clinic
 
