02949c0f0315541e3de5e537a8e1acd62e96b7fc
{tmoshole} <{tmoshole@student.wethinkcode.co.za}>
2020-11-27 10:45:49 +0200

    update

--- main/code/codebase/book_slot.py
+++ main/code/codebase/book_slot.py
@@ -43,6 +43,36 @@ def create_doctor_event(start, summary, pat_email):
     print("Event created:", result.get("summary"))
 
 
+def is_slot_avalaible(events, year, month, day, time):
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                        maxResults=100, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+    #print (events_result, " vvvevent result")
+    coo = len(events) - 1
+    for v in range(10
+    ):
+        print (events[v], " ========================================================= ")
+        coo -= 1
+    if not events:
+        return True
+    for i in events:           
+        full_start_date = i['start'].get('dateTime').split("+")
+        print (full_start_date)
+        print ("------------------------------------------------------------------")
+        start_date_time = full_start_date[0].split("T")
+        start_year = start_date_time[0].split('-')[0]
+        start_month = start_date_time[0].split('-')[1]
+        start_day = start_date_time[0].split('-')[2]
+        start_hour = ''.join(start_date_time[1].split('-')).split(':')[0]
+        start_minute = ''.join(start_date_time[1].split('-')).split(':')[1]
+        start_time = start_hour + ':' + start_minute
+        if start_year == year and start_month == month and start_day == day and start_time == time:
+            return False
+    return True
+
+
 def validate_token():
     # username = input_API.book_doctor(list_)
     if os.path.exists(f"{os.environ['HOME']}/.config/.clinic/username.txt"):
@@ -99,9 +129,13 @@ def main():
     day = input("slot day: ")
     slot_time = f'{year} {month} {day} {time}'
     pat_email = input_API.book_patient(list_)
+    if is_slot_avalaible(service, year, month, day, time) == False:
+            print('Double booking')
+    else: 
+        create_doctor_event(slot_time, topic, pat_email)
 
-    create_doctor_event(slot_time, topic, pat_email)
 
+    
 
 
 if __name__ == '__main__':
