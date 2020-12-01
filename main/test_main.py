5b0f0dc28c85f67610545963cbe7830344f06a43
ndumasi <ndumasi@student.wethinkcode.co.za>
2020-12-01 18:32:38 +0200

    Unittesting done

--- main/test_main.py
+++ main/test_main.py
@@ -7,12 +7,12 @@ import unittest
 
 class Test_Project(unittest.TestCase):
     def test_help(self):
-        value = subprocess.getoutput('python3 -m unittest code/help_cc_/test_help.py')
+        value = subprocess.getoutput('python3 -m unittest main/code/help_cc_/test_help.py')
         self.assertNotIn('FAILED',value,msg='\n\n\n\nTEST_HELP FAILED')
         # print(value)
     
     def test_input(self):
-        value = subprocess.getoutput('python3 -m unittest code/input_cc_/test_inputs.py')
+        value = subprocess.getoutput('python3 -m unittest main/code/input_cc_/test_inputs.py')
         self.assertNotIn('FAILED',value,msg='\n\n\n\nTEST_INPUTS FAILED')
         print(value)
     
